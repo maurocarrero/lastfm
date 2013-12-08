731e47d947c405b49dbcf7be7f1c139d1da2c357
maurocarrero <maurocarrero@adinet.com.uy>
2013-12-08 20:45:33 -0200

    Adds Artist details, routing, delete innecesary code

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,19 @@
 var lastfmApp = angular.module('lastfmApp', [
 	'lastfmControllers',
-	// 'lastfmServices'
+	'ngRoute'
 ]);
+
+lastfmApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/artist_details/:mbid', {
+			templateUrl: 'partials/artist_details.html',
+			controller: 'ArtistDetailsController'
+		})
+		.when('/artist_search', {
+				templateUrl: 'partials/artist_search.html',
+				controller: 'ArtistSearchController'
+		})
+		.otherwise({
+			redirectTo: '/artist_search'
+		})
+}]);
--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,37 +1,58 @@
 
-var lastfmControllers = angular.module('lastfmControllers', []);
+var lastfmControllers = angular.module('lastfmControllers', [])
+	
+	.controller('ArtistSearchController',['$scope', '$http', '$location',
+		function($scope, $http, $location) {
+			$scope.artistmatches = [];
+			
+			$scope.searchArtist = function() {
+				$http({
+					url: 'http://ws.audioscrobbler.com/2.0/?',
+					method: 'GET',
+					params: {
+							artist: $scope.search_string,
+							method: 'artist.search',
+							api_key: '9cef1044431cd99f7af6db2051d36ba2',
+			    			format: 'json'
+						}
+					})
+					.success(function(data, status, headers, config) {
+						var artistMatches = data.results.artistmatches.artist;
+						for (index in artistMatches) {
+							if (artistMatches[index].mbid !== '') {
+								$scope.artistmatches.push(artistMatches[index]); 
+							}
+						}
+					})
+					.error(function(data, status, headers, config) {
 
-lastfmControllers.factory('ArtistFactory', ['Artist', function(Artist){
-	var data = Artist.query();
+					});
+			};
 
-	var factory = {};
-	factory.getArtists = function() {
-		return data;
-	}
-	return factory;
-}]);
+		$scope.artistDetails = function(mbid) {
+			$location.path('/artist_details/' + mbid).replace();
+		}
+	}])
 
-lastfmControllers.controller('ArtistController', 
-	function($scope, $http) {
-	// $scope.artists = ArtistFactory.getArtists();
+	.controller('ArtistDetailsController', ['$scope', '$http', '$routeParams', '$sce', 
+		function($scope, $http, $routeParams, $sce) {
 
-	$scope.searchArtist = function() {
 		$http({
 			url: 'http://ws.audioscrobbler.com/2.0/?',
 			method: 'GET',
 			params: {
-					artist: $scope.search_string,
-					method: 'artist.search',
+					mbid: $routeParams.mbid,
+					method: 'artist.getinfo',
 					api_key: '9cef1044431cd99f7af6db2051d36ba2',
 	    			format: 'json'
 				}
 			})
 			.success(function(data, status, headers, config) {
-				$scope.artistmatches = data.results.artistmatches.artist;
+				data.artist.summary = $sce.trustAsHtml(JSON.stringify(data.artist.summary));
+				$scope.artist = data.artist;
 			})
 			.error(function(data, status, headers, config) {
 
-			});
-	};
-	
-});
+			})
+
+	}]);
